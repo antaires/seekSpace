06252e8baf4117fc9e11ecd9fc59719f517623c7
antaires <11vdlo@gmail.com>
2019-05-23 11:23:39 +0100

    added sonam main page

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,94 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+<div id="app">
+<div class="wrapper" v-bind:style="bgc">
+  <div class="content">
+    <h1>{{welcome}}</h1>
+    <h2>{{ description }}</h2>
+    <input type="text" v-on:input="bgc.backgroundColor = $event.target.value" placeholder="Type here..."/>
+  <div>
+    <br>
+    <br>
+    <h1>Meet Our Friendly Staff Below</h1>
+  <v-app id="inspire">
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-flex d-flex xs12 sm6 md4>
+          <v-card hover color=cyan darken-2>
+            <v-card-title primary class="title">Pilot</v-card-title>
+            <v-icon x-large dark>flight_takeoff</v-icon>
+            <v-card-text>{{ pilot }}</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex d-flex xs12 sm6 md3>
+          <v-layout row wrap>
+            <v-flex d-flex>
+              <v-card color=indigo darken-4>
+                <v-card-title primary class="title">Co-Pilot</v-card-title>
+                    <v-icon x-large dark>android</v-icon>
+                <v-card-text>{{ filler.slice(0, 70) }}</v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex d-flex>
+              <v-layout row wrap>
+                <v-flex
+                  v-for="n in 2"
+                  :key="n"
+                  d-flex
+                  xs12
+                >
+                  <v-card
+                    color=deep-purple lighten-1
+                  >
+                    <v-card-text>{{ filler.slice(0, 40) }}</v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex d-flex xs12 sm6 md2 child-flex>
+          <v-card color=brown lighten-2>
+            <v-card-text>{{ filler.slice(0, 90) }}</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex d-flex xs12 sm6 md3>
+          <v-card color=blue-grey darken-1>
+            <v-card-text>{{ filler.slice(0, 100) }}</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+    </div>
   </div>
+</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      description: 'Type any colour name, rgb, hex or hsl in the input below to customise your homepage!',
+      welcome: 'Welcome to seekSpace',
+      bgc: {
+        backgroundColor: ''
+      },
+      pilot: `Pilot Hans Gruber has been with us since 2198! ` +
+             `He will make sure our flight is as asteroid free as possible.` +
+             `You can find him on the flight deck programming his trusty ` +
+             `AI Co-Pilot Rob`,
+      filler: `Staff says hello`
     }
   }
 }
@@ -96,18 +87,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+},
+.v-card-title {
+  color: azure
 }
 </style>
