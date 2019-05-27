aae4debd473e66b212b4f1e0395d1fa5a0e9f7a5
antaires <11vdlo@gmail.com>
2019-05-27 17:05:10 +0100

    rooms can be accessed from database and selected room shown

--- client/src/components/Rooms.vue
+++ client/src/components/Rooms.vue
@@ -2,6 +2,11 @@
   <div>
     <div v-if="$store.state.isUserLoggedIn && $store.state.room">
       <panel title="You have selected a room: ">
+        <div v-for="room in rooms" :key="room.roomId">
+        <div v-if="room.roomId == $store.state.room">
+          <span class="room-name"> {{room.name}} </span>
+        </div>
+        </div>
       </panel>
     </div>
 
@@ -9,6 +14,7 @@
       <span v-if="!$store.state.isUserLoggedIn"> Please sign up or login to view rooms! </span>
       <span v-if="$store.state.isUserLoggedIn"> -- WELCOME! -- </span>
 
+      <div v-if="$store.state.isUserLoggedIn">
       <div v-for="room in rooms" :key="room.roomId">
         <div class="room">
           <v-flex>
@@ -21,9 +27,13 @@
             <div class="room-detail">
               {{room.detail}}
             </div>
+
+          <v-flex>
             <img class="room-imageURL" :src="room.imageURL"/>
           </v-flex>
+          </v-flex>
         </div>
+      </div>
       </div>
 
     </panel>
@@ -40,45 +50,6 @@ export default {
   data () {
     return {
       rooms: null
-      /*
-      rooms: [
-        {
-          roomId: 1,
-          name: 'Eros',
-          feature: 'twin bed and en-suite',
-          imageURL: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/06/14/neptune-planet-space.jpg',
-          detail: 'One Nepture year lasts 164.8 Earth years. In fact, since it was discovered in 1846, Nepture has only copmleted 1 year, in 2011'
-        },
-        {
-          roomId: 2,
-          name: 'Andromeda',
-          feature: 'Queen size bed and double-wide galactic view window',
-          imageURL: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/06/14/neptune-planet-space.jpg',
-          detail: 'One Nepture year lasts 164.8 Earth years. In fact, since it was discovered in 1846, Nepture has only copmleted 1 year, in 2011'
-        },
-        {
-          roomId: 3,
-          name: 'Enceladus',
-          feature: 'two full rooms and kitchen',
-          imageURL: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/06/14/neptune-planet-space.jpg',
-          detail: 'One Nepture year lasts 164.8 Earth years. In fact, since it was discovered in 1846, Nepture has only copmleted 1 year, in 2011'
-        },
-        {
-          roomId: 4,
-          name: 'Dione',
-          feature: 'single bed and shared bath',
-          imageURL: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/06/14/neptune-planet-space.jpg',
-          detail: 'One Nepture year lasts 164.8 Earth years. In fact, since it was discovered in 1846, Nepture has only copmleted 1 year, in 2011'
-        },
-        {
-          roomId: 5,
-          name: 'Rhea',
-          feature: 'honeymoon suite',
-          imageURL: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/06/14/neptune-planet-space.jpg',
-          detail: 'One Nepture year lasts 164.8 Earth years. In fact, since it was discovered in 1846, Nepture has only copmleted 1 year, in 2011'
-        }
-      ]
-      */
     }
   },
   async mounted () {
@@ -92,8 +63,6 @@ export default {
 
 .room {
   padding: 20px;
-  height: 330px;
-  overflow: hidden;
 }
 
 .room-name {
